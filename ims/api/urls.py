3fddffb21dbfa739d54e73f1784e20968ef28cec
muslumcanozata <muslumcanozata@gmail.com>
2021-05-14 20:46:10 +0300

    SarfKullanicilarDetailsList API eklendi.

--- ims/api/urls.py
+++ ims/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from ims.api import views as api_views
 
 urlpatterns = [
-    path('sarfKullanicilar/', api_views.sarfKullanicilarListCreateAPIView.as_view(), name='Sarf Kullanıcılar Listesi')
+    path('sarfKullanicilar/', api_views.sarfKullanicilarListCreateAPIView.as_view(), name='Sarf Kullanıcılar Listesi'),
+    path('sarfKullanicilar/<int:pk>', api_views.sarfKullanicilarDetailsListCreateAPIView.as_view(), name='Sarf Kullanıcılar Detay Listesi')
 ]
--- ims/api/views.py
+++ ims/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 from ims.models import sarfKullanicilarM
 from ims.api.serializers import sarfKullanicilarSerializer
 
@@ -23,3 +24,21 @@ class sarfKullanicilarListCreateAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class sarfKullanicilarDetailsListCreateAPIView(APIView):
+    
+    def get_object(self, pk):
+        sarfKullanicilar_instance = get_object_or_404(sarfKullanicilarM, pk=pk)
+        return sarfKullanicilar_instance
+    
+    def get(self, request, pk):
+        sarfKullanicilar = self.get_object(pk=pk)
+        serializer = sarfKullanicilarSerializer(sarfKullanicilar)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        sarfKullanicilar =self.get_object(pk=pk)
+        serializer = sarfKullanicilarSerializer(sarfKullanicilar, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
