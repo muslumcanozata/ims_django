5601cfe3cd0f9b1ea0b8600e4a199294c41b5764
Müslüm Can <muslumcanozata@gmail.com>
2021-05-17 00:27:49 +0300

    SQLite'a geçildi.

--- config/settings.py
+++ config/settings.py
@@ -75,14 +75,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ims',
+#         'USER': 'mco',
+#         'PASSWORD': '051192mcaN.',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'imsv1.1',
-        'USER': 'django',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
