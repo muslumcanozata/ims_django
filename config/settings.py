732f0aad611fe233d56ed5fc9fee50d037f4a561
Müslüm Can <muslumcanozata@gmail.com>
2021-05-25 14:50:41 +0300

    REST API izin isteği düzenlendi.

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,8 @@ CORS_ORIGIN_WHITELIST = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        "rest_framework.permissions.AllowAny",
+        #'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
