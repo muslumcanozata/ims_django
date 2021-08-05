86dc60a29857d91e8c53e1b541ab46b606634f00
muslumcanozata <mcan.ozata@gmail.com>
2021-08-05 13:56:41 +0300

    requirements and db upgraded

--- config/settings.py
+++ config/settings.py
@@ -78,21 +78,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ims',
-#         'USER': 'mco',
-#         'PASSWORD': '051192mcaN.',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
